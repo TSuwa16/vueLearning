931b6440fd66fc5867e1b368e0be4455ec1de4b7
Tsuwa16 <tk.suwa16@gmail.com>
2020-11-15 12:03:02 +0900

    try:vuetify

---
+++ vuetify/myapp/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-list>
+    <template v-for="todo in todos">
+      <v-list-tile v-bind:key="todo.id">
+        <v-list-tile-content>
+          {{todo.text}}
+        </v-list-tile-content>
+        <v-list-tile-action>
+          <v-btn flat icon v-on:click="deleteTodo(todo.id)">
+            <v-icon>delete</v-icon>
+          </v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
+      <v-divider v-bind:key="todo.id"></v-divider>
+    </template>> 
+  </v-list>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      todos:[
+        {id:"1",text:"todo 1"},
+        {id:"2",text:"todo 2"},
+        {id:"2",text:"todo 3"},
+      ]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos = this.todos.filter((todo => todo.id !== id));
+    }
+  }
+
+}
+</script>
